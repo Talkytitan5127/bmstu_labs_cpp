3658076c16bfbc8202553f59e38a32ed38d18c98
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2017-03-22 19:54:08 +0300

    Create CastException.h

---
+++ module2/lab6/CastException.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <string>
+
+class CastException
+	: public std::exception
+{
+public:
+	virtual void what() = 0;
+};
+
+class LengthError
+	: public CastException
+{
+	void what()
+	{
+		std::cout << "Error!!! Out of int range." << '\n';
+	}
+};
+
+class ErrorData
+	: public CastException
+{
+	void what()
+	{
+		std::cout << "Error!!. The original date isn't type of function." << '\n';
+	}
+};
